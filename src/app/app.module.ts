64c432bd0f73dc16b52ac543a95f6ed51578a22b
Smita Nargundkar <nsmitanode@gmail.com>
2019-04-14 20:16:41 +0530

    SPA part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,10 @@ import { DishService } from './services/dish.service';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
