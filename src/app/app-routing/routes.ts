bbcd8c279925b7129472a705c2faeae3126fa3bb
Smita Nargundkar <nsmitanode@gmail.com>
2019-04-16 14:44:16 +0530

    assignment 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,5 +11,6 @@ export const routes: Routes = [
   { path: 'menu',     component: MenuComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
    { path: 'dishdetail/:id',     component: DishdetailComponent },
-  { path: 'contactus',     component: ContactComponent }
+  { path: 'contactus',     component: ContactComponent },
+  { path: 'aboutus',     component: AboutComponent }
 ];
