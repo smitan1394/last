d35bc6c08af024b4488d0cc4ce62eeb350e4abe1
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-06-21 17:57:38 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(./dist));
-// Start the app by listening on the default
+app.use(express.static(__dirname + '/dist/conFusion'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + `/dist/conFusion/index.html`)); // load the single view file (angular will handle the page changes on the front-end)
+});
 // Heroku port
 app.listen(process.env.PORT || 8080);
