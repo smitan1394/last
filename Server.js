6af1481542d6b38e6d1b5951a71c00afb6f50c7a
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-06-21 18:14:00 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
+app.use(express.static(__dirname + 'db.json'));
 app.use(express.static(__dirname + '/dist/conFusion'));
 
 app.get('*', (req, res) => {
