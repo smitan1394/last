b19a04a2f10954193b1cb2e14265b3f752ce5287
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-06-24 16:07:51 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -4,9 +4,8 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/db.json'));
 
-//app.use(express.static(__dirname + '/dist/conFusion'));
+app.use(express.static(__dirname + '/dist/conFusion'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + `/dist/conFusion/index.html`)); // load the single view file (angular will handle the page changes on the front-end)
