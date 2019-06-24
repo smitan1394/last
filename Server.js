d393ad6f5fdbea2a3a7f84af044c5f938cb5fbae
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-06-24 12:49:47 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + 'db.json'));
+app.use(bodyParser.json(express.static(__dirname + '/db.json')));
+
 app.use(express.static(__dirname + '/dist/conFusion'));
 
 app.get('*', (req, res) => {
