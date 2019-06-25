189e10f6a7251e2bbc8afd9db9e91aece52c80eb
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-06-25 17:21:13 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-var jsonServer = require('json-server');
-
-app.use('/api', jsonServer.defaults(), jsonServer.router('db.json'));
-app.use(express.static(__dirname + '/dist/conFusion'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + `/dist/conFusion/index.html`)); // load the single view file (angular will handle the page changes on the front-end)
