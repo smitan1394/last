b26dfdfedce76149c212c34e1df950f86f7253df
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-06-25 14:28:51 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
 
 app.use(express.static(__dirname + '/dist/conFusion'));
 
