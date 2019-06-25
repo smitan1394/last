55709907e089fa8154b291c3e7cac21da5d82e6a
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-06-25 14:56:19 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 var jsonServer = require('json-server');
 
-app.use('/', jsonServer.router('db.json'));
+app.use('/api', jsonServer.defaults(), jsonServer.router('db.json'));
 app.use(express.static(__dirname + '/dist/conFusion'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + `/dist/conFusion/index.html`)); // load the single view file (angular will handle the page changes on the front-end)
 });
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
 
